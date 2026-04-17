afc4f3e67723283be8b587985736dce8a0f331ce
Virab_RoBots <114412453+VirabRoBots@users.noreply.github.com>
2026-04-17 23:00:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,6 +48,11 @@ AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Second
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
 
+# Auth Channel Information
+AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "")  # Channel username or ID (without @ for username)
+AUTH_CHANNEL_MODE = is_enabled(environ.get('AUTH_CHANNEL_MODE', "True"), True)  # Set True to enable force subscribe
+VERIFY_CHANNEL_LINK = environ.get("VERIFY_CHANNEL_LINK", "")  # Channel invite link (optional)
+
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
